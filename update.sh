de1e98a38d4746185e09c756cbefaf2c028c2fc4
WickedTree <50127946+WickedTree@users.noreply.github.com>
2020-03-25 11:27:03 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 cd ~
 declare -a lines
 readarray -t lines ghcredentials.txt
-git clone https://github.com/WickedTree/odle-hangupsbot.git newhb https://${lines[0]}:${lines[1]}@github.com
+git clone https://${lines[0]}:${lines[1]}@https://github.com/WickedTree/odle-hangupsbot.git newhb
 sudo pkill python
 rm -rf hangoutsbot
 mv newhb hangoutsbot
